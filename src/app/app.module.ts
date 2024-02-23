51f7ccbcffaff54d1964cb0429de2e1e871ca98a
dlecoeur <dorian.lecoeur@tessi.fr>
2024-02-23 23:21:31 +0100

    Correction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,10 @@ import {HttpClientModule} from '@angular/common/http';
 import {RealColdComponent} from './real-cold/real-cold.component';
 import {WithLatestFromComponent} from './pipe/with-latest-from/with-latest-from.component';
 import {PipeOrderComponent} from './pipe/pipe-order/pipe-order.component';
-import {CasPratiqueComponent} from './cas-pratique/cas-pratique.component';
 import {IifComponent} from './pipe/iif/iif.component';
 import {UnsubscribeComponent} from './unsubscribe/unsubscribe.component';
 import {CombineLatestComponent} from './combine-latest/combine-latest.component';
 import {InputComponent} from './pipe/input/input.component';
-import {MapComponent} from './pipe/map/map.component';
-import {CatchErrorComponent} from './pipe/catch-error/catch-error.component';
 import {MergemapComponent} from './pipe/mergemap/mergemap.component';
 import {SwitchMapComponent} from './pipe/switch-map/switch-map.component';
 
@@ -29,13 +26,10 @@ import {SwitchMapComponent} from './pipe/switch-map/switch-map.component';
     RealColdComponent,
     WithLatestFromComponent,
     PipeOrderComponent,
-    CasPratiqueComponent,
     IifComponent,
     UnsubscribeComponent,
     CombineLatestComponent,
     InputComponent,
-    MapComponent,
-    CatchErrorComponent,
     MergemapComponent,
     SwitchMapComponent
   ],
